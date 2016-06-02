f7a4c000d672f48d2c76cd9aa9e70c980e45d83d
Tom Hudson <mail@tomnomnom.com>
2016-06-02 23:30:31 +0100

    Moves statement related code to new files, adds comments

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"regexp"
 	"sort"
 )
 
@@ -47,117 +46,6 @@ func main() {
 	for _, s := range ss {
 		fmt.Println(s)
 	}
-}
-
-func keyMustBeQuoted(s string) bool {
-	r := regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9]*$`)
-	return !r.MatchString(s)
-}
-
-func makePrefix(prev string, next interface{}) (string, error) {
-	switch v := next.(type) {
-	case int:
-		return fmt.Sprintf("%s[%d]", prev, v), nil
-	case string:
-		if keyMustBeQuoted(v) {
-			return fmt.Sprintf("%s[%s]", prev, escape(v)), nil
-		} else {
-			return fmt.Sprintf("%s.%s", prev, v), nil
-		}
-	default:
-		return "", fmt.Errorf("could not form prefix for %#v", next)
-	}
-}
-
-type statements []string
-
-func (ss *statements) Add(prefix, value string) {
-	*ss = append(*ss, fmt.Sprintf("%s = %s;", prefix, value))
-}
-
-func (ss *statements) AddGroup(g statements) {
-	*ss = append(*ss, g...)
-}
-
-func (ss statements) Len() int {
-	return len(ss)
-}
-
-func (ss statements) Swap(i, j int) {
-	ss[i], ss[j] = ss[j], ss[i]
-}
-
-func (ss statements) Less(i, j int) bool {
-	return ss[i] < ss[j]
-}
-
-func (ss statements) Contains(search string) bool {
-	for _, i := range ss {
-		if i == search {
-			return true
-		}
-	}
-	return false
-}
-
-func makeStatements(prefix string, v interface{}) (statements, error) {
-	ss := make(statements, 0)
-
-	switch vv := v.(type) {
-
-	case map[string]interface{}:
-		// It's an object
-		ss.Add(prefix, "{}")
-
-		for k, sub := range vv {
-			newPrefix, err := makePrefix(prefix, k)
-			if err != nil {
-				return ss, err
-			}
-			extra, err := makeStatements(newPrefix, sub)
-			if err != nil {
-				return ss, err
-			}
-			ss.AddGroup(extra)
-		}
-
-	case []interface{}:
-		// It's an array
-		ss.Add(prefix, "[]")
-
-		for k, sub := range vv {
-			newPrefix, err := makePrefix(prefix, k)
-			if err != nil {
-				return ss, err
-			}
-			extra, err := makeStatements(newPrefix, sub)
-			if err != nil {
-				return ss, err
-			}
-			ss.AddGroup(extra)
-		}
-
-	case float64:
-		ss.Add(prefix, escape(vv))
-
-	case string:
-		ss.Add(prefix, escape(vv))
-
-	case bool:
-		ss.Add(prefix, fmt.Sprintf("%t", vv))
-
-	case nil:
-		ss.Add(prefix, "null")
-	}
-
-	return ss, nil
-}
-
-func escape(s interface{}) string {
-	// I'm pretty sure it's safe to ignore this error
-	// ...maybe. I'll work something into this I promise
-	out, _ := json.Marshal(s)
-	return string(out)
 }
 
 func todo(err error) {
---
+++ statements.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"regexp"
+)
+
+// statements is a list of assignment statements.
+// E.g statement: json.foo = "bar";
+type statements []string
+
+// Add adds a new statement to the list given the prefix and value
+func (ss *statements) Add(prefix, value string) {
+	*ss = append(*ss, fmt.Sprintf("%s = %s;", prefix, value))
+}
+
+// AddMulti adds a whole other list of statements
+func (ss *statements) AddMulti(l statements) {
+	*ss = append(*ss, l...)
+}
+
+// Len returns the number of statements for sort.Sort
+func (ss statements) Len() int {
+	return len(ss)
+}
+
+// Swap swaps two statements for sort.Sort
+func (ss statements) Swap(i, j int) {
+	ss[i], ss[j] = ss[j], ss[i]
+}
+
+// Less compares two statements for sort.Sort
+func (ss statements) Less(i, j int) bool {
+	return ss[i] < ss[j]
+}
+
+// Contains seaches the statements for a given statement
+// Mostly to make testing things easier
+func (ss statements) Contains(search string) bool {
+	for _, i := range ss {
+		if i == search {
+			return true
+		}
+	}
+	return false
+}
+
+// makeStatements takes a prefix and interface value and returns
+// a statements list or an error on failure
+func makeStatements(prefix string, v interface{}) (statements, error) {
+	ss := make(statements, 0)
+
+	switch vv := v.(type) {
+
+	case map[string]interface{}:
+		// It's an object
+		ss.Add(prefix, "{}")
+
+		for k, sub := range vv {
+			newPrefix, err := makePrefix(prefix, k)
+			if err != nil {
+				return ss, err
+			}
+			extra, err := makeStatements(newPrefix, sub)
+			if err != nil {
+				return ss, err
+			}
+			ss.AddMulti(extra)
+		}
+
+	case []interface{}:
+		// It's an array
+		ss.Add(prefix, "[]")
+
+		for k, sub := range vv {
+			newPrefix, err := makePrefix(prefix, k)
+			if err != nil {
+				return ss, err
+			}
+			extra, err := makeStatements(newPrefix, sub)
+			if err != nil {
+				return ss, err
+			}
+			ss.AddMulti(extra)
+		}
+
+	case float64:
+		ss.Add(prefix, formatValue(vv))
+
+	case string:
+		ss.Add(prefix, formatValue(vv))
+
+	case bool:
+		ss.Add(prefix, fmt.Sprintf("%t", vv))
+
+	case nil:
+		ss.Add(prefix, "null")
+	}
+
+	return ss, nil
+}
+
+// formatValue uses json.Marshal to format scalars
+// E.g:
+//     a string -> "a string"
+//     7.0000   -> 7
+func formatValue(s interface{}) string {
+	// I'm pretty sure it's safe to ignore this error
+	// ...maybe. I'll work something into this I promise
+	out, _ := json.Marshal(s)
+	return string(out)
+}
+
+// keyMustBeQuoted checks to see if a key in a JSON object
+// must be quoted or not.
+// E.g:
+//     justLettersAndNumbers1 -> false
+//     a key with spaces      -> true
+//     1startsWithANumber	  -> true
+func keyMustBeQuoted(s string) bool {
+	r := regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9]*$`)
+	return !r.MatchString(s)
+}
+
+// makePrefix takes the previous prefix and the next key and
+// returns a new prefix or and error on failure
+func makePrefix(prev string, next interface{}) (string, error) {
+	switch v := next.(type) {
+	case int:
+		return fmt.Sprintf("%s[%d]", prev, v), nil
+	case string:
+		if keyMustBeQuoted(v) {
+			return fmt.Sprintf("%s[%s]", prev, formatValue(v)), nil
+		}
+		return fmt.Sprintf("%s.%s", prev, v), nil
+	default:
+		return "", fmt.Errorf("could not form prefix for %#v", next)
+	}
+}
