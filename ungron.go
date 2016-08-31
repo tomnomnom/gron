5c8ce26d30fb9a523e7b24ea12077c58ccb4aa3c
Tom Hudson <mail@tomnomnom.com>
2016-09-01 00:28:33 +0100

    Moves grammar spec to comment in ungron.go

--- ungron.go
+++ ungron.go
@@ -1,3 +1,15 @@
+// Ungronning is the reverse of gronning: turn statements
+// back into JSON. The expected input grammar is:
+//
+//   Input ::= '--'* Statement (Statement | '--')*
+//   Statement ::= Path Space* "=" Space* Value ";" "\n"
+//   Path ::= (BareWord) ("." BareWord | ("[" Key "]"))*
+//   BareWord ::= (UnicodeLu | UnicodeLl | UnicodeLm | UnicodeLo | UnicodeNl | '$' | '_') (UnicodeLu | UnicodeLl | UnicodeLm | UnicodeLo | UnicodeNl | UnicodeMn | UnicodeMc | UnicodeNd | UnicodePc | '$' | '_')*
+//   Key ::= [0-9]+ | String
+//   Value ::= String | Number | "true" | "false" | "null" | "[]" | "{}"
+//   String ::= '"' (UnescapedRune | ("\" (["\/bfnrt] | ('u' Hex))))* '"'
+//   UnescapedRune ::= [^#x0-#x1f"\]
+
 package main
 
 import (
