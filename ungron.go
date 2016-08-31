3980828df98267afebdb745e98d7d5db0c8a1c4a
Tom Hudson <mail@tomnomnom.com>
2016-09-01 00:39:23 +0100

    Reorders EBNF in docs

--- ungron.go
+++ ungron.go
@@ -4,9 +4,9 @@
 //   Input ::= '--'* Statement (Statement | '--')*
 //   Statement ::= Path Space* "=" Space* Value ";" "\n"
 //   Path ::= (BareWord) ("." BareWord | ("[" Key "]"))*
+//   Value ::= String | Number | "true" | "false" | "null" | "[]" | "{}"
 //   BareWord ::= (UnicodeLu | UnicodeLl | UnicodeLm | UnicodeLo | UnicodeNl | '$' | '_') (UnicodeLu | UnicodeLl | UnicodeLm | UnicodeLo | UnicodeNl | UnicodeMn | UnicodeMc | UnicodeNd | UnicodePc | '$' | '_')*
 //   Key ::= [0-9]+ | String
-//   Value ::= String | Number | "true" | "false" | "null" | "[]" | "{}"
 //   String ::= '"' (UnescapedRune | ("\" (["\/bfnrt] | ('u' Hex))))* '"'
 //   UnescapedRune ::= [^#x0-#x1f"\]
 
