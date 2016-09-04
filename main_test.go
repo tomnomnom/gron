2320f9c84a5df0867065132d891f6e9ad28037c8
Tom Hudson <mail@tomnomnom.com>
2016-09-04 10:38:07 +0100

    Adds ungron tests to main_test

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"os"
 	"reflect"
@@ -46,4 +47,56 @@ func TestGron(t *testing.T) {
 		}
 	}
 
+}
+
+func TestUngron(t *testing.T) {
+	cases := []struct {
+		inFile  string
+		outFile string
+	}{
+		{"testdata/one.gron", "testdata/one.json"},
+		{"testdata/two.gron", "testdata/two.json"},
+		{"testdata/three.gron", "testdata/three.json"},
+	}
+
+	for _, c := range cases {
+		wantF, err := ioutil.ReadFile(c.outFile)
+		if err != nil {
+			t.Fatalf("failed to open want file: %s", err)
+		}
+
+		var want interface{}
+		err = json.Unmarshal(wantF, &want)
+		if err != nil {
+			t.Fatalf("failed to unmarshal JSON from want file: %s", err)
+		}
+
+		in, err := os.Open(c.inFile)
+		if err != nil {
+			t.Fatalf("failed to open input file: %s", err)
+		}
+
+		out := &bytes.Buffer{}
+		code, err := ungron(in, out, true)
+
+		if code != exitOK {
+			t.Errorf("want exitOK; have %d", code)
+		}
+		if err != nil {
+			t.Errorf("want nil error; have %s", err)
+		}
+
+		var have interface{}
+		err = json.Unmarshal(out.Bytes(), &have)
+		if err != nil {
+			t.Fatalf("failed to unmarshal JSON from ungron output: %s", err)
+		}
+
+		if !reflect.DeepEqual(want, have) {
+			t.Logf("want: %#v", want)
+			t.Logf("have: %#v", have)
+			t.Errorf("ungronned %s does not match %s", c.inFile, c.outFile)
+		}
+
+	}
 }
